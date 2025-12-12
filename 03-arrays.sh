e3f3cd5885c1ab2ce7442dd3319a27e7257fb191
Peelamdeepika <deepikapeelam16@gmail.com>
2025-12-12 22:24:53 +0530

    testing

---
+++ 03-arrays.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+Family="Father" "Mother" "borther" " sister"
+
+echo " My family is more fear of : ${FAMILY[0]}"
+echo " My family is more taken care by : ${FAMILY[1]}"
+echo "my ${FAMILY[2]} is more restriced person to ${FAMILY[3]}"
