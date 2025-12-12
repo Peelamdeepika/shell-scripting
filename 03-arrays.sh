a25d3630bc3baadb6f9013830d0672e1e50ef25f
Peelamdeepika <deepikapeelam16@gmail.com>
2025-12-12 22:27:28 +0530

    testing

--- 03-arrays.sh
+++ 03-arrays.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-Family=( "Father" "Mother" "borther" " sister" )
+FAMILY=( "Father" "Mother" "borther" " sister" )
 
-echo " My family is more fear of : ${FAMILY[0]}"
-echo " My family is more taken care by : ${FAMILY[1]}"
-echo "my ${FAMILY[2]} is more restriced person to ${FAMILY[3]}"
+echo "My family is more fear of : ${FAMILY[0]}"
+echo "My family is more taken care by : ${FAMILY[1]}"
+echo "My ${FAMILY[2]} is more restriced person to ${FAMILY[3]}"
