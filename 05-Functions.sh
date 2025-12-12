edf4ca6147eebc1bb75eaa36bc2a4f9709074228
Peelamdeepika <deepikapeelam16@gmail.com>
2025-12-12 23:32:22 +0530

    testing

---
+++ 05-Functions.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+R="\[31m"
+G="\e{32m"
+Y="\e[33m"
+
+FOLDER="/var/log/shell-scripting-logs"
+FILENAME=05-Function
+TIMESTAMP=$(date +%y-%m-%d)
+FILE="$FOLDER/$FILENAME-$TIMESTAMP.log"
+
+VALIDATE() {
+   if [ $? -ne 0 ]; then
+     dnf install $PACKAGE 
+    else
+      echo -e "$PACAKGE installed $Y successfully"
+}
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+  echo "you need root access" &>>$FILE
+  exit 1
+fi 
+
+for PACKAGE in 
+do
+  dnf list installed $PACKAGE
+  if [ $? -ne 0 ]; then
+   dnf install $PACKAGE -y 
+   VALIDATE $1 "in your server"
+  else
+  echo -e "$G mysql is already installed in your server"  &>>$FILE
+  fi 
+done
