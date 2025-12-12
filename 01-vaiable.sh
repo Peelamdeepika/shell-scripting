73576710042e39641b529c547ab3eab8e9ae8930
Peelamdeepika <deepikapeelam16@gmail.com>
2025-12-12 22:10:28 +0530

    testing

--- 01-vaiable.sh
+++ 01-vaiable.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo " hello world"
+PLACE=Kolkata
+NAME1=Deepika
+NAME2=Srijit
+
+echo "$NAME1 : Hi $NAME2"
+echo "$NAME2 : Hi $NAME1"
+echo "$NAME1 : hey $NAME2 i am coming t $PLACE"
+echo "$NMAE2 : IS it, I am more exited $NAME1"
