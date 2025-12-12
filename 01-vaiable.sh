ada6d4934aec61d7a19ce05911686002de9f4853
Peelamdeepika <deepikapeelam16@gmail.com>
2025-12-12 22:10:58 +0530

    testing

--- 01-vaiable.sh
+++ 01-vaiable.sh
@@ -6,4 +6,4 @@ NAME2=Srijit
 echo "$NAME1 : Hi $NAME2"
 echo "$NAME2 : Hi $NAME1"
 echo "$NAME1 : hey $NAME2 i am coming t $PLACE"
-echo "$NMAE2 : IS it, I am more exited $NAME1"
+echo "$NAME2 : IS it, I am more exited $NAME1"
