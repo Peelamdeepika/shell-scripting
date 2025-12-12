0b9cbee19264efbfc18a724fa69deab6b92856b7
Peelamdeepika <deepikapeelam16@gmail.com>
2025-12-12 22:43:43 +0530

    testing

--- 04-condition.sh
+++ 04-condition.sh
@@ -7,13 +7,13 @@ Y="\e[33m
 
 
 if [ $USERID -ne 0 ]; then
-  echo "Now yor are not root user"
+  echo "Now yo are not root user"
   exit 1
 fi 
 
 yum install mysql -y
 
-if [ $? -ne 0 ]
+if [ $? -ne 0 ]; then
    echo "mysql is not $R installed in your server"
     exit 1
 else
